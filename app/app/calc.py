ef1ee66d8e0dc259a15d80eaacfd35f64265f067
drKeyes <mike.keeman@gmail.com>
2023-10-18 19:16:15 +0100

    postgresql adaptor added to docker

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x,y):
+    """Add x and y and return results."""
+    return x + y
+
+def sub(x,y):
+    """Add x and y and return results."""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Add numbers together and return result"""
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        """Substract second number from first number and return result"""
+        res = calc.sub(10, 4)
+
+        self.assertEqual(res, 6)
